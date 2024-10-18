23ddc2a3737c783e73daee4d45c90f457960ec25
logic <logic@solidleisure.com>
2024-10-18 14:37:20 +0800

    xxx

---
+++ utils/haiwei.go
@@ -0,0 +1,17 @@
+package utils
+
+function HwAPI(){
+    // resp, err := http.Get("https://api.example.com/data")
+    // if err != nil {
+    //     log.Printf("Error fetching data: %v", err)
+    //     return
+    // }
+    // defer resp.Body.Close()
+
+    // 解析 JSON 数据
+    //var result map[string]interface{}
+    //if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+    //    log.Printf("Error decoding JSON: %v", err)
+    //    return
+    //}
+}
